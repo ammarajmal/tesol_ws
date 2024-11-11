dba40bae0aab97068c702b31f5e049572a78c657
Ammar Ajmal <ammarajml@gmail.com>
2024-11-11 13:53:15 +0900

    gazebo dir

--- build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -8,9 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ammar/tesol_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH='/home/ammar/ros_ws/devel/lib:/opt/ros/noetic/lib'
-export PKG_CONFIG_PATH='/home/ammar/ros_ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/ammar/tesol_ws/build/catkin_tools_prebuild'
-export PYTHONPATH='/home/ammar/ros_ws/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/ammar/tesol_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH='/home/ammar/tesol_ws/build/catkin_tools_prebuild:/home/ammar/ros_ws/src/test_pkg:/home/ammar/tesol_ws/src/camera_info_manager_py:/home/ammar/tesol_ws/src/charuco_calibration/charuco_calibration:/home/ammar/tesol_ws/src/fiducials/fiducial_msgs:/home/ammar/tesol_ws/src/fiducials/fiducial_slam:/home/ammar/tesol_ws/src/fiducials/fiducials:/home/ammar/tesol_ws/src/sony_cam:/home/ammar/tesol_ws/src/tesol_detect:/home/ammar/tesol_ws/src/vision_msgs:/home/ammar/tesol_ws/src/fiducials/aruco_detect:/home/ammar/tesol_ws/src/fiducials/stag_detect:/opt/ros/noetic/share'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ammar/tesol_ws/devel/.private/catkin_tools_prebuild/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ammar/tesol_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
--- devel/.private/tesol_detect/share/tesol_detect/cmake/tesol_detectConfig.cmake
+++ devel/.private/tesol_detect/share/tesol_detect/cmake/tesol_detectConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ammar/tesol_ws/devel/.private/tesol_detect/lib;/home/ammar/ros_ws/devel/lib;/home/ammar/tesol_ws/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/ammar/tesol_ws/devel/.private/tesol_detect/lib;/home/ammar/tesol_ws/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
